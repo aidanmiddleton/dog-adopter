4156673882a37b1698b181df07a5dbe3234bbc30
Aidan Middleton <aidanemiddleton@gmail.com>
2020-07-01 16:54:16 -0700

    added cardview with rodummy data, implmented rotation effect

---
+++ Dog Adopter/Dog Adopter/CardView.swift
@@ -0,0 +1,71 @@
+//
+//  CardView.swift
+//  Dog Adopter
+//
+//  Created by Aidan Middleton on 2020-07-01.
+//  Copyright © 2020 Aidan Middleton. All rights reserved.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    
+    @State private var translation: CGSize = .zero
+    
+    var body: some View {
+        // 1
+        GeometryReader { geometry in
+                        
+            // 2
+            VStack(alignment: .leading) {
+                Image("dog 3")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: geometry.size.width, height: geometry.size.height * 0.75) // 3
+                    .clipped()
+                
+                HStack {
+                    VStack(alignment: .leading, spacing: 6) {
+                        Text("Debra Weber, 28")
+                            .font(.title)
+                            .bold()
+                        Text("Judge")
+                            .font(.subheadline)
+                            .bold()
+                        Text("13 Mutual Friends")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                    }
+                    Spacer() // Add a spacer to push our HStack to the left and the spacer fill the empty space
+                    
+                    Image(systemName: "info.circle")
+                        .foregroundColor(.gray)
+                }.padding(.horizontal)
+            }
+            // Add padding, corner radius and shadow with blur radius
+            .padding(.bottom)
+            .background(Color.white)
+            .cornerRadius(10)
+            .shadow(radius: 5)
+            .offset(x: self.translation.width, y: 0) // 2
+            .rotationEffect(.degrees(Double(self.translation.width / geometry.size.width) * 25), anchor: .bottom)
+            .gesture(
+                // 3
+                DragGesture()
+                    // 4
+                    .onChanged { value in
+                        self.translation = value.translation
+                    }.onEnded { value in
+                        self.translation = .zero
+                    }
+            )
+        }
+    }
+}
+
+// 4
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView().frame(height: 400).padding()
+    }
+}
--- Dog Adopter/Dog Adopter/ContentView.swift
+++ Dog Adopter/Dog Adopter/ContentView.swift
@@ -10,7 +10,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+       // 1
+        VStack {
+            DateView()
+        }.padding()
+    }
+}
+
+// 2
+struct DateView: View {
+    var body: some View {
+      // Container to add background and corner radius to
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Friday, 10th January")
+                        .font(.title)
+                        .bold()
+                    Text("Today")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+                Spacer()
+            }.padding()
+        }
+        .background(Color.white)
+        .cornerRadius(10)
+        .shadow(radius: 5)
     }
 }
 
